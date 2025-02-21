7647b765dcb4944bcf80dd6704c1edb4dce27545
studio1member <megame0113@gmail.com>
2025-02-22 02:08:28 +0700

    init

--- nav/nav.js
+++ nav/nav.js
@@ -1,28 +1,28 @@
 fetch("../nav/nav.html")
-    .then(Response  => Response.text())
-    .then(data =>{
-        document.getElementById("nav-placeholder").innerHTML = data;
-    })
+.then(Response  => Response.text())
+.then(data =>{
+    document.getElementById("nav-placeholder").innerHTML = data;
+})
 
-    function toggleMenu() {
-        var menu = document.getElementById("menu");
-        var menuOff = document.getElementById("menu-off");
-        if (menu.style.left === "0px") {
-            menu.style.left = "-220px"; // Đóng menu
-            menuOff.style.left = "-1000px";
-        } else {
-            menu.style.left = "0px"; // Mở menu
-            menuOff.style.left = "0px";
-        }
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    var menuOff = document.getElementById("menu-off");
+    if (menu.style.left === "0px") {
+        menu.style.left = "-220px"; // Đóng menu
+        menuOff.style.left = "-1000px";
+    } else {
+        menu.style.left = "0px"; // Mở menu
+        menuOff.style.left = "0px";
     }
-    function toggleMenuOff() {
-        var menu = document.getElementById("menu");
-        var menuOff = document.getElementById("menu-off");
-        if (menu.style.left === "0px") {
-            menu.style.left = "-220px"; // Đóng menu
-            menuOff.style.left = "-1000px";
-        } else {
-            menu.style.left = "0px"; // Mở menu
-            menuOff.style.left = "0px";
-        }
+}
+function toggleMenuOff() {
+    var menu = document.getElementById("menu");
+    var menuOff = document.getElementById("menu-off");
+    if (menu.style.left === "0px") {
+        menu.style.left = "-220px"; // Đóng menu
+        menuOff.style.left = "-1000px";
+    } else {
+        menu.style.left = "0px"; // Mở menu
+        menuOff.style.left = "0px";
     }
+}
